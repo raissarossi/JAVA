921241a7aa7cc7012c25e8a334595d8669df0c9b
raissarossi <109825210+raissarossi@users.noreply.github.com>
2022-09-16 16:38:17 -0300

    Add files via upload

---
+++ JogoMagico/src/Jogo.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Jogo {
+//        int Vencedor(int[][][] velha){
+//            int a=2;
+//            return velha;
+//        }
+    public static void main(String[] args) {
+        int[][] velhaT1 = {{10, 20, 30} , {40, 50, 60} , {70, 80, 90}};
+        int[] l0 = velhaT1[0];
+        int[] l1 = velhaT1[1];
+        int[] l2 = velhaT1[2];
+        int[] c0 = {velhaT1[0][0], velhaT1[1][0], velhaT1[2][0]};
+        int[] c1 = {velhaT1[0][1], velhaT1[1][1], velhaT1[2][1]};
+        int[] c2 = {velhaT1[0][2], velhaT1[1][2], velhaT1[2][2]};
+        int[] d0 = {velhaT1[0][0], velhaT1[1][1], velhaT1[2][2]};
+        int[] d1 = {velhaT1[0][2], velhaT1[1][1], velhaT1[2][0]};
+
+
+
+
+
+
+
+
+
+
+    }
+}
