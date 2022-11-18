ba21f1abe566971749f14f9d541cadd7e78be16b
Rai Rossi <109825210+raissarossi@users.noreply.github.com>
2022-11-18 09:27:37 -0300

    Add files via upload

---
+++ JOGO_VELHA_CUBO/jogoMagico/src/bosch/Main.java
@@ -0,0 +1,11 @@
+package bosch;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+        jogo.jogar();
+    }
+
+}
---
+++ JOGO_VELHA_CUBO/jogoMagico/src/bosch/Teste.java
@@ -0,0 +1,19 @@
+package bosch;
+
+import java.util.Arrays;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        Velha teste = new Velha();
+        Velha testa0 = new Velha();
+        Velha[] tudo = {teste, testa0};
+        int[] linha = {1,2,3};
+        System.out.println(Arrays.toString(teste.getLine(0)));
+        tudo[0].setLine(0,linha);
+        System.out.println(Arrays.toString(tudo[0].getLine(0)));
+        System.out.println(Arrays.toString(teste.getLine(0)));
+
+
+    }
+}
