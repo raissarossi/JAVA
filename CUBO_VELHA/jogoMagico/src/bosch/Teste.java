a412cfb2fb63c0e65571dddff4837f2fd01f76dc
Rai Rossi <109825210+raissarossi@users.noreply.github.com>
2022-11-25 08:06:45 -0300

    Add files via upload

---
+++ CUBO_VELHA/jogoMagico/src/bosch/Teste.java
@@ -0,0 +1,15 @@
+package bosch;
+
+import java.util.Arrays;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        Velha teste = new Velha();
+        Velha testa0 = new Velha();
+        Velha[] tudo = {teste, testa0};
+
+
+
+    }
+}
